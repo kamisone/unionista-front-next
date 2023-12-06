64391f9168d96dea36f7a8186d9af993cda1401d
neoxia <mbk@gmail.com>
2023-12-06 08:36:40 +0100

    remove orphans when down

--- start_front.sh
+++ start_front.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker compose --file /srv/front/current/docker-compose.yml down
+docker compose --file /srv/front/current/docker-compose.yml down --remove-orphans
 docker compose --file /srv/front/current/docker-compose.yml up --build
