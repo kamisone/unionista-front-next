a6dc53702c9132817e8482c4154d71e2e37e5708
neoxia <mbk@gmail.com>
2023-12-06 11:15:32 +0100

    do not create network

--- start_front.sh
+++ start_front.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 docker compose --file /srv/front/current/docker-compose.yml down --remove-orphans
-docker network inspect unionistashop_network >/dev/null || docker network create --driver bridge unionistashop_network
+NETWORK_NAME=unionistashop_network
+if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
+     docker network create --driver bridge ${NETWORK_NAME} ; 
+fi
 docker compose --file /srv/front/current/docker-compose.yml up --build
