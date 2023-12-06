cfab784a464ce6a9aaf36f12a19a80acc25e1243
neoxia <mbk@gmail.com>
2023-12-06 11:11:26 +0100

    create network when it do not exist

--- start_front.sh
+++ start_front.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker compose --file /srv/front/current/docker-compose.yml down --remove-orphans
-docker network remove unionistashop_network
-docker network create --driver bridge unionistashop_network
+docker network inspect unionistashop_network >/dev/null || docker network create --driver bridge unionistashop_network
 docker compose --file /srv/front/current/docker-compose.yml up --build
