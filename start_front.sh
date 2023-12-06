b43bd546583c72dec5634d54e9637950a4af0be6
neoxia <mbk@gmail.com>
2023-12-06 10:51:14 +0100

    created unionstashopNetwork when creating project

--- start_front.sh
+++ start_front.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker compose --file /srv/front/current/docker-compose.yml down --remove-orphans
+docker network remove unionistashop_network
+docker network create --driver bridge unionistashop_network
 docker compose --file /srv/front/current/docker-compose.yml up --build
