9c75d885a67b40985518e41f615575b0acf710b5
neoxia <mbk@gmail.com>
2024-01-01 19:03:56 +0100

    remove orphans if postgres do not exist

--- start_front.sh
+++ start_front.sh
@@ -4,4 +4,7 @@ NETWORK_NAME=unionistashop_network
 if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
      docker network create --driver bridge ${NETWORK_NAME} ; 
 fi
+if [ -z $(docker ps --filter name=postgres --format="{{ .Names }}") ] ; then 
+     docker compose --file /srv/back/current/docker-compose.yml down --remove-orphans
+fi
 docker compose --file /srv/front/current/docker-compose.yml up --build
