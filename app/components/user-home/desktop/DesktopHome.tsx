c4120abd19451ab714987c1cbdff474b22945b18
mbenkhouya <mbenkhouya@fr-d7d40c2hyg.home>
2024-02-28 20:38:29 +0100

    Initialized DesktopHome component

--- app/components/user-home/desktop/DesktopHome.tsx
+++ app/components/user-home/desktop/DesktopHome.tsx
@@ -1,5 +1,8 @@
 import { SupportedLanguages } from '@/app/i18n/settings';
 import React from 'react';
+import styles from '@/app/components/user-home/desktop/DesktopHome.module.css';
+import clsx from 'clsx';
+import Link from 'next/link';
 
 interface DesktopHomeProps {
     lng: SupportedLanguages;
@@ -7,7 +10,18 @@ interface DesktopHomeProps {
 }
 
 const DesktopHome = ({ lng, isUserAuthenticated }: DesktopHomeProps) => {
-    return <h2>Desktop body</h2>;
+    return (
+        <div className={clsx(styles.container)}>
+            <nav className={styles.category_nav}>
+                <ul>
+                    <li><Link href={'home-favorites'} />Home Favorites</li>
+                    <li><Link href={'fashion-favorites'} />Fashion</li>
+                    <li><Link href={'gifts'} />Gifts</li>
+                </ul>
+            </nav>
+            <span></span>
+        </div>
+    )
 };
 
 export default DesktopHome;
