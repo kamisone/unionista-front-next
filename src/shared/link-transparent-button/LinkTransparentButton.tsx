10f583a3a252834e43a2ac3218667a744c9fa113
mbk <mohamed.benkhouya@neoxia.com>
2024-09-05 21:42:47 +0200

    added spinner on click

--- src/shared/link-transparent-button/LinkTransparentButton.tsx
+++ src/shared/link-transparent-button/LinkTransparentButton.tsx
@@ -1,24 +1,41 @@
 'use client';
-import { getProtectedPaths } from '@/config';
 import { PENDING_REDIRECT_PATH_NAME } from '@/utils/constants';
 import { useRouter } from 'next/navigation';
-import React, { ReactNode } from 'react';
-import { Cookies } from 'react-cookie';
+import { ReactNode, useEffect, useState, useTransition } from 'react';
+import LoadingIndicator from '../loading-indicator/LoadingIndicator';
+import styles from './LinkTransparentButton.module.css';
 
 interface LinkTransparentButtonProps {
     to: string;
-    isProtected?: boolean
+    isProtected?: boolean;
     children: ReactNode;
 }
 
-export default function LinkTransparentButton(props: LinkTransparentButtonProps) {
+export default function LinkTransparentButton(
+    props: LinkTransparentButtonProps
+) {
     const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+    const [isPending, startTransition] = useTransition();
+    const [isTriggered, setIsTriggered] = useState(false);
+
+    useEffect(() => {
+        if(isTriggered && !isPending) {
+            setIsLoading(false);
+        }
+    }, [isPending])
     return (
         <button
+            className={styles.container}
             onClick={() => {
-                router.push(props.to);
+                setIsLoading(true);
+                startTransition(() => {
+                    setIsTriggered(true);
+                    router.push(props.to);
+                });
+                
                 if (
-                    props.isProtected && 
+                    props.isProtected &&
                     document.cookie.includes(PENDING_REDIRECT_PATH_NAME) &&
                     !document.cookie.includes(
                         `${PENDING_REDIRECT_PATH_NAME}=${props.to}`
@@ -30,6 +47,12 @@ export default function LinkTransparentButton(props: LinkTransparentButtonProps)
             }}
         >
             {props.children}
+
+            {isLoading && (
+                <div className={styles.spinner}>
+                    <LoadingIndicator />
+                </div>
+            )}
         </button>
     );
 }
--- src/shared/modal-spot/ModalSpot.tsx
+++ src/shared/modal-spot/ModalSpot.tsx
@@ -1,21 +1,11 @@
 import { Graphik, UthmanicFont } from '@/fonts/fonts';
 import { SupportedLanguages, SupportedLanguagesEnum } from '@/i18n/settings';
 import CloseIcon from '@/icons/close-icon/CloseIcon';
-import { AuthService } from '@/services/auth.service';
-import { ModalService } from '@/services/modal.service';
-import ScaleBgWrapper from '@/shared/scale-bg-wrapper/ScaleBgWrapper';
 import clsx from 'clsx';
-import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { ReactNode } from 'react';
-import './ModalSpot.css';
 import LinkTransparentButton from '../link-transparent-button/LinkTransparentButton';
-// import ScaleBgWrapper from 'shared/scale-bg-wrapper/ScaleBgWrapper';
-
-const authService = AuthService.instance;
-const modalService = ModalService.instance;
-
-// interface ModalService extends ComponentsStateNotify<any, any> {}
+import './ModalSpot.css';
 
 interface ModalSpotProps {
     children: ReactNode[] | ReactNode | string;
@@ -35,58 +25,13 @@ function ModalSpot({
     animationDirection = 'animate_to_top',
     isDesktop = false,
 }: ModalSpotProps) {
-    // const { t } = useTranslation();
-    // const [isDrawerQuitting, setIsDrawerQuitting] = useState(false);
-    // const [ModalContent, setModalContent] = useState(
-    //     modalService.state.currentModalContent
-    // );
-
-    // const onCloseDrawer = (duration = 300) => {
-    //     // if (
-    //     //     (
-    //     //         [
-    //     //             ModalContentMapping.SIGN_IN,
-    //     //             ModalContentMapping.SIGN_UP,
-    //     //         ] as (ModalContentMapping | null)[]
-    //     //     ).includes(bottomModalContent)
-    //     // ) {
-    //     //     authService.state = {
-    //     //         isUserNotifiedToSignin: true,
-    //     //     };
-    //     // }
-    //     setIsDrawerQuitting(true);
-    //     setTimeout(() => {
-    //         modalService.state = {
-    //             isModalOpen: false,
-    //             currentModalContent: null,
-    //         };
-    //     }, duration);
-    // };
-
-    // set notifiers
-    // useEffect(() => {
-    //     modalService.addNotifier(
-    //         (options) =>
-    //             options && setModalContent(options.state.currentModalContent)
-    //     );
-    // }, []);
-
-    function removeQueryParam(path: string, param: string) {
-        // path.replace()
-    }
-
     return (
         <div
             className={clsx('ms_container', animationDirection, {
-                // ms_quitting: isDrawerQuitting,
                 is_desktop: isDesktop,
             })}
-            // onClick={() => onCloseDrawer()}
         >
-            <div
-                className={clsx('ms_content')}
-                // onClick={(e) => e.stopPropagation()}
-            >
+            <div className={clsx('ms_content')}>
                 <div className={clsx({ ms_title_container: !!headingTitle })}>
                     {headingTitle && (
                         <>
@@ -98,27 +43,12 @@ function ModalSpot({
                                         : Graphik.className
                                 )}
                             >
-                                {/* {t('drawer.title')} */}
                                 {headingTitle}
                             </h2>
                         </>
                     )}
-                    {/* <Link
-                        href={`/${lng}`}
-                        className={clsx('ms_close_icon', {
-                            floating: !headingTitle,
-                        })}
-                    >
-                        <ScaleBgWrapper
-                            Icon={<CloseIcon />}
-                            // onClick={() => onCloseDrawer()}
-                        />
-                    </Link> */}
                     <LinkTransparentButton to={`/${lng}`}>
-                        <ScaleBgWrapper
-                            Icon={<CloseIcon />}
-                            // onClick={() => onCloseDrawer()}
-                        />
+                        <CloseIcon />
                     </LinkTransparentButton>
                 </div>
                 {children}
