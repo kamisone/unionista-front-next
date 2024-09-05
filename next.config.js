7a28f93bd0c2326c5e1edf708d2a60af98535955
mbk <mohamed.benkhouya@neoxia.com>
2024-09-05 10:57:34 +0200

    disable nginx buffering

--- next.config.js
+++ next.config.js
@@ -28,6 +28,19 @@ const nextConfig = {
     //         }
     //     ]
     // }
+    async headers() {
+        return [
+            {
+                source: '/:path*{/}?',
+                headers: [
+                    {
+                        key: 'X-Accel-Buffering',
+                        value: 'no',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
