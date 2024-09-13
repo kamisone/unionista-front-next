d9a926e78f6bcb92a6ac169d523601dd38cb8e6b
mbk <mohamed.benkhouya@neoxia.com>
2024-09-13 06:32:01 +0200

    removed comments from i18n files

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,43 +1,6 @@
-// import i18n, { Namespace, createInstance, InitOptions } from 'i18next';
-// import { SupportedLanguages, defaultNs, getOptions } from '@/i18n/settings';
-// import { initReactI18next } from 'react-i18next/initReactI18next';
-// import resourcesToBackend from 'i18next-resources-to-backend';
-// import i18nextFSBackend from 'i18next-fs-backend';
-// // import { I18n, InitPromise, CreateClientReturn } from 'next-i18next';
-
 import { isBrowser } from '@/utils/is-browser';
 import { SupportedLanguages } from './settings';
 import { getCookies } from '@/utils/query-params';
-
-// const initI18next = async (lng: SupportedLanguages, ns: Namespace) => {
-//     const i18nInstance = createInstance();
-//     await i18nInstance
-//         .use(initReactI18next)
-//         .use(
-//             resourcesToBackend(
-//                 (language: string, namespace: string) =>
-//                     import(`@/i18n/locales/${language}/${namespace}.json`)
-//             )
-//         )
-//         .init(getOptions(lng, ns));
-//     return i18nInstance;
-// };
-
-// export async function useTranslation(
-//     lng: SupportedLanguages,
-//     ns: Namespace = defaultNs,
-//     options: any = {}
-// ) {
-//     const i18nextInstance = await initI18next(lng, ns);
-//     return {
-//         t: i18nextInstance.getFixedT(
-//             lng,
-//             Array.isArray(ns) ? ns[0] : ns,
-//             options?.keyPrefix
-//         ),
-//         i18n: i18nextInstance,
-//     };
-// }
 
 const DEFAULT_NAME_SPACE = 'translation';
 
--- src/i18n/settings.ts
+++ src/i18n/settings.ts
@@ -1,5 +1,3 @@
-// import { Namespace } from 'i18next';
-
 export const fallbackLng = 'en';
 
 export type SupportedLanguages = 'en' | 'fr' | 'es' | 'ar';
@@ -13,18 +11,3 @@ export enum SupportedLanguagesEnum {
 export const languages: SupportedLanguages[] = [fallbackLng, 'es', 'fr', 'ar'];
 
 export const lngCookieName = 'lng';
-// export const defaultNs = 'translation';
-
-// export function getOptions(
-//     lng: SupportedLanguages = fallbackLng,
-//     ns: Namespace = defaultNs
-// ) {
-//     return {
-//         supportedLngs: languages,
-//         fallbackLng,
-//         lng,
-//         fallbackNs: defaultNs,
-//         defaultNs,
-//         ns,
-//     };
-// }
