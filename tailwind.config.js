3a36eeac8979d66087c69ffc7fbc920288fb75fb
mbk <mohamed.benkhouya@neoxia.com>
2024-09-20 14:28:44 +0200

    nothing changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
                 primary: '#f1641e',
                 secondary: '#222222',
                 white: 'white',
+
                 'light-black-overlay': 'rgba(0, 0, 0, 0.2)',
                 'dark-gray-overlay': '#0e0e0e2e',
                 'medium-black-overlay': 'rgba(0, 0, 0, 0.5)',
