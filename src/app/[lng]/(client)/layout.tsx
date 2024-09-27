026c56d122a5a30428e4a14867dec11b3b6592a2
mbk <mohamed.benkhouya@neoxia.com>
2024-09-27 13:50:29 +0200

    remove cientheader from admin part

--- src/app/[lng]/(client)/layout.tsx
+++ src/app/[lng]/(client)/layout.tsx
@@ -1,7 +1,10 @@
 import styles from '@/app/[lng]/(client)/layout.module.css';
 import ClientFooter from '@/components/footer/ClientFooter';
+import ClientHeader from '@/components/header/ClientHeader';
 import { UthmanicFont } from '@/fonts/fonts';
 import { SupportedLanguages, SupportedLanguagesEnum } from '@/i18n/settings';
+import { JwtPayload } from '@/services/types/auth';
+import { CURRENT_USER_PAYLOAD_HEADER_NAME } from '@/utils/constants';
 import { isMobile } from '@/utils/is-browser';
 import clsx from 'clsx';
 import { headers } from 'next/headers';
@@ -16,12 +19,22 @@ async function ClientLayout({ children, params: { lng } }: ClientLayoutProps) {
     const headersList = headers();
     const isMobileDevice = isMobile(headersList.get('user-agent'));
 
+    const userPayload: JwtPayload | null =
+        (headersList.get(CURRENT_USER_PAYLOAD_HEADER_NAME) &&
+            JSON.parse(headersList.get(CURRENT_USER_PAYLOAD_HEADER_NAME)!)) ||
+        null;
+
     return (
         <main
             className={clsx(lng, styles.container, {
                 [UthmanicFont.className]: lng === SupportedLanguagesEnum.AR,
             })}
         >
+            <ClientHeader
+                isMobile={isMobileDevice}
+                lng={lng}
+                userPayload={userPayload}
+            />
             {children}
             <ClientFooter isMobile={isMobileDevice} lng={lng} />
         </main>
